2ccd02ff403db0bfd9de6134309acde36a3ab20d
parkJH24 <jh.parkssam@gmail.com>
2024-01-02 10:41:48 +0900

    dfdhdf

--- server/server.js
+++ server/server.js
@@ -1,38 +1,26 @@
+// 네이버 검색 API 예제 - 블로그 검색
 var express = require('express');
-var request = require('request'); // 파일 상단으로 이동
 var app = express();
-
-// 환경 변수 사용으로 보안 강화
-var client_id = process.env.NAVER_CLIENT_ID;
-var client_secret = process.env.NAVER_CLIENT_SECRET;
-
+var client_id = process.env.REACT_APP_NAVER_ID;
+var client_secret = process.env.REACT_APP_NAVER_SECRET;
 app.get('/search/blog', function (req, res) {
-   if (!req.query.query) {
-       return res.status(400).send('Query parameter is required');
-   }
-
-   var api_url = 'https://openapi.naver.com/v1/search/book?query=' + encodeURI(req.query.query);
+   var api_url = 'https://openapi.naver.com/v1/search/book?query=' + encodeURI(req.query.query); // JSON 결과
+//   var api_url = 'https://openapi.naver.com/v1/search/blog.xml?query=' + encodeURI(req.query.query); // XML 결과
+   var request = require('request');
    var options = {
        url: api_url,
        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
     };
-
    request.get(options, function (error, response, body) {
-     if (error) {
-       console.error('Request error:', error);
-       return res.status(500).send('Internal Server Error');
+     if (!error && response.statusCode == 200) {
+       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+       res.end(body);
+     } else {
+       res.status(response.statusCode).end();
+       console.log('error = ' + response.statusCode);
      }
-     
-     if (response.statusCode !== 200) {
-       console.error('API error:', response.statusCode);
-       return res.status(response.statusCode).send('Error from API');
-     }
-
-     res.setHeader('Content-Type', 'application/json;charset=utf-8');
-     res.send(body);
    });
-});
-
-app.listen(3000, function () {
-   console.log('Server is running on port 3000. Go to http://127.0.0.1:3000/search/blog?query=검색어');
-});
+ });
+ app.listen(3000, function () {
+   console.log('http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!');
+ });
