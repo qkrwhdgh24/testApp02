b853744b71684b1f2eaaf501182e430db362664b
parkJH24 <jh.parkssam@gmail.com>
2024-01-02 15:09:27 +0900

    tryry

--- src/index.js
+++ src/index.js
@@ -1,63 +1,40 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import MyBook from './pages/MyBook';
+import Phrase from './pages/Phrase';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
-function SearchBook() {
-    const [keywords, setKeywords] = useState('');
-    const [bookResults, setBookResults] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-    useEffect(() => {
-        console.log(bookResults); // Log the state when it updates
-    }, [bookResults]);
+//const routes = createBrowserRouter([
+const routes = createBrowserRouter([
+  {
+    path : '/', 
+    element : <App/>,
+    errorElement : <ErrorPage />,
 
-    const bookSearchKeyword = (e) => {
-        setKeywords(e.target.value);
-    }
+    children : [
+      {path : '/login', element : <LogIn/>},
+      {path : '/signup', element : <SignUp/>},
+      {path : '/mybook' , element : <MyBook/>},
+      {path : '/phrase', element : <Phrase/>},
+    ]
+  }
+])
 
-    const searchBookEvent = async () => {
-        if (!keywords.trim()) {
-            console.log('검색어 없음');
-            return;
-        }
-        setIsLoading(true);
-        setError(null);
-        try {
-            const apiUrl = `/src/SeachBook?query=${encodeURIComponent(keywords)}`;
-            // const apiUrl = `/?query=${encodeURIComponent(keywords)}`;
-            const res = await axios.get(apiUrl);
-            if (res.headers['content-type']?.includes('application/json')) {
-                const { data } = res;
-                setBookResults(data);
-            } else {
-                console.error(res);    
-                setError('no data');
-            }
-            
-        } catch (error) {
-            console.error(error);
-            setError('no data');
-        } finally {
-            setIsLoading(false);
-        }
-    }
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={routes}/>
+  </React.StrictMode>
+);
 
-    return (
-        <div>
-            <input 
-                value={keywords}
-                onChange={bookSearchKeyword}
-            />
-            <button onClick={searchBookEvent}>검색</button>
-            {isLoading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            {Array.isArray(bookResults) && bookResults.map((book, index) => (
-            <div key={index}>
-                <p>{book.title}</p>
-            </div>
-        ))}
-        </div>
-    )
-}
-
-export default SearchBook;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
